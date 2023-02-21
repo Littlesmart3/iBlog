dc2233184af7fac036abcaaf7ccc38171608961b
范力 <798347235@qq.com>
2023-02-21 21:35:02 +0800

    feat: 修改文案

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,31 +1,34 @@
-import React from "react";
-import clsx from "clsx";
-import styles from "./styles.module.css";
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: "Coder",
-    Svg: require("@site/static/img/item2.svg").default,
-    description: <>Design is a process of finding problems, not solutions.</>,
+    title: 'Coder',
+    Svg: require('@site/static/img/item2.svg').default,
+    description: <>设计是发现问题的过程，而不是解决方案。</>,
   },
   {
-    title: "Gamer",
-    Svg: require("@site/static/img/league_of_legends.svg").default,
-    description: <>Never stop playing If not fun, why bother?</>,
-  },
-  {
-    title: "Sporter",
-    Svg: require("@site/static/img/sport.svg").default,
+    title: 'Gamer',
+    Svg: require('@site/static/img/league_of_legends.svg').default,
     description: (
       <>
-        There is nothing more beautiful than strong muscle and fresh skin of his
-        cloak.
+        没关系的，大家都会做错选择，会莫名其妙掉眼泪，但这并不影响我打游戏，来不来
+      </>
+    ),
+  },
+  {
+    title: 'Camera',
+    Svg: require('@site/static/img/camera.svg').default,
+    description: (
+      <>
+        想拍就拍，想照就照，再好的相机和科技，10年后也恢复不了当下的场景和互动的情绪。
       </>
     ),
   },
@@ -33,7 +36,7 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import logo from "@site/static/img/头像-mini.png";
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import logo from '@site/static/img/头像-mini.png';
 
-import styles from "./index.module.css";
+import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx(styles.heroBanner)}>
       <div className="container">
-        <img src={logo} style={{ width: 280, borderRadius: "50%" }} />
+        <img src={logo} style={{ width: 280, borderRadius: '50%' }} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">Never Knows Best</p>
         <div className={styles.buttons}></div>
